ef447f94e2913b1c00c635039037a6af487c0b65
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2017-10-23 09:24:28 +0200

    Check for rc file, bail out if does not exist.

--- src/su-dump-conversation.sh
+++ src/su-dump-conversation.sh
@@ -3,8 +3,19 @@ UA='User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.
 SU_MAIN_URL='https://showup.tv/site/log_in?ref=https://showup.tv/TransList/fullList/lang/pl'
 SU_LOGIN_URL='https://showup.tv/site/log_in'
 SU_COOKIE_JAR=~/.su-cookies.txt
+SU_MSGTOOLS_RC=~/.su-messaging-tools-rc
 
-. ~/.su-messaging-tools-rc
+echo_stderr() {
+	(>&2 echo "$@")
+}
+
+
+if [ -f ${SU_MSGTOOLS_RC} ] ; then
+	. ${SU_MSGTOOLS_RC}
+else
+	echo_stderr ${SU_MSGTOOLS_RC} does not exist.
+	exit 10
+fi
 
 curl -H "$UA" -v --data-raw "email=$SU_LOGIN" -d "password=$SU_PASS" -d "submitLogin=Zaloguj" --cookie 'accept_rules=true' -e $SU_LOGIN_URL -c $SU_COOKIE_JAR -X POST $SU_MAIN_URL
 curl -H "$UA" -v --cookie "accept_rules=true" -b $SU_COOKIE_JAR https://showup.tv/site/messages/get_talk/${SU_CONVERSATION_ID}
