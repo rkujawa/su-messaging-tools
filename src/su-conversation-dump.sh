7feb850d02ab3dfb96fccbdc3cda51bd6b3ca192
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2017-10-24 22:58:02 +0200

    Split common code into separate file.

--- src/su-conversation-dump.sh
+++ src/su-conversation-dump.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 # This script dumps recent messages in a specified conversation, in JSON format.
 # Requires fairly recent curl.
+. $(dirname $0)/su-conversation.subr
+
 UA='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 SU_URL='https://showup.tv/'
 SU_TRANS_URL="${SU_URL}TransList/fullList/lang/pl"
 SU_TRANS_LOGIN_URL="${SU_URL}site/log_in?ref=$SU_TRANS_URL"
 SU_LOGIN_URL="${SU_URL}site/log_in"
 SU_GET_TALK_URL="${SU_URL}site/messages/get_talk/"
-SU_COOKIE_JAR=~/.su-cookies.txt
-SU_MSGTOOLS_RC=~/.su-messaging-tools-rc
+SU_COOKIE_JAR=${SU_MSGTOOLS_CFGDIR}/cookies.txt
 SU_MSGTOOLS_WORKDIR=/run/user/${UID}/su-messaging-tools
 SU_MSGTOOLS_LOGIN_HEADERS=${SU_MSGTOOLS_WORKDIR}/login.headers
 SU_ADD_COOKIES="--cookie accept_rules=true"
 
 usage() {
 	echo $0 'login|talk-get conversation-id'
-}
-
-echo_stderr() {
-	(>&2 echo "$@")
 }
 
 # Log in, obtain session cookie and save it to $SU_COOKIE_JAR.
@@ -56,12 +53,6 @@ su_talk_get() {
 
 }
 
-if [ -f ${SU_MSGTOOLS_RC} ] ; then
-	. ${SU_MSGTOOLS_RC}
-else
-	echo_stderr ${SU_MSGTOOLS_RC} does not exist.
-	exit 10
-fi
 
 case "$1" in
 	login)
