829f81c1d841b855a1a1adc9c1e4084dbc490496
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2017-10-24 10:13:49 +0200

    Split login and chat retrieval.

--- src/su-dump-conversation.sh
+++ src/su-dump-conversation.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
+# This script dumps recent messages in a specified conversation, in JSON format.
+# Requires fairly recent curl.
 UA='User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
-SU_MAIN_URL='https://showup.tv/site/log_in?ref=https://showup.tv/TransList/fullList/lang/pl'
-SU_LOGIN_URL='https://showup.tv/site/log_in'
+SU_URL='https://showup.tv/'
+SU_TRANS_URL="${SU_URL}site/log_in?ref=https://showup.tv/TransList/fullList/lang/pl"
+SU_LOGIN_URL="${SU_URL}site/log_in"
+SU_GET_TALK_URL="${SU_URL}site/messages/get_talk/"
 SU_COOKIE_JAR=~/.su-cookies.txt
 SU_MSGTOOLS_RC=~/.su-messaging-tools-rc
+SU_ADD_COOKIES="--cookie accept_rules=true"
+
+usage() {
+	echo $0 'login|talk-get conversation-id'
+}
 
 echo_stderr() {
 	(>&2 echo "$@")
 }
 
+su_login() {
+
+
+	local SU_LOGIN_POST_DATA="--data-raw email=$SU_LOGIN -d password=$SU_PASS -d submitLogin=Zaloguj"
+
+	curl -H "$UA" -v $SU_LOGIN_POST_DATA $SU_ADD_COOKIES -e $SU_LOGIN_URL -c $SU_COOKIE_JAR -X POST $SU_TRANS_URL
+
+}
+
+su_talk_get() {
+	curl -H "$UA" -v $SU_ADD_COOKIES -b $SU_COOKIE_JAR ${SU_GET_TALK_URL}${SU_CONVERSATION_ID}
+}
 
 if [ -f ${SU_MSGTOOLS_RC} ] ; then
 	. ${SU_MSGTOOLS_RC}
@@ -17,6 +38,16 @@ else
 	exit 10
 fi
 
-curl -H "$UA" -v --data-raw "email=$SU_LOGIN" -d "password=$SU_PASS" -d "submitLogin=Zaloguj" --cookie 'accept_rules=true' -e $SU_LOGIN_URL -c $SU_COOKIE_JAR -X POST $SU_MAIN_URL
-curl -H "$UA" -v --cookie "accept_rules=true" -b $SU_COOKIE_JAR https://showup.tv/site/messages/get_talk/${SU_CONVERSATION_ID}
+case "$1" in
+	login)
+		su_login
+		;;
+	talk-get)
+		su_talk_get
+		;;
+	*)
+		usage
+		exit 5
+		;;
+esac
 
